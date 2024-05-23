7cef0601e874192d0e027fc8a4b8679d3ea6f335
Jeremiah-Katumo <jeremykatush@gmail.com>
2024-05-23 15:21:20 +0300

    added content in UserContext component

--- src/components/context/UserContext.tsx
+++ src/components/context/UserContext.tsx
@@ -1,5 +1,28 @@
 
+import React, { createContext, useState } from "react"
+
 export type AuthUserC = {
     name: string
     email: string
+}
+
+type UserContextType = {
+    user: AuthUserC | null
+    setUser:  React.Dispatch<React.SetStateAction<AuthUserC | null>>
+}
+
+type UserContextProviderProps = {
+    children: React.ReactNode
+}
+
+export const UserContext = createContext<UserContextType | null>(null)
+
+export const UserContextProvider = ({ children }: UserContextProviderProps) => {
+    const [user, setUser] = useState<AuthUserC | null>(null)
+
+    return (
+        <UserContext.Provider value={{ user, setUser }}>
+            { children }
+        </UserContext.Provider>
+    )
 }
