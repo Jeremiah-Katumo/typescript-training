87455c3ca11d646737cd11477116e77b302eef93
Jeremiah-Katumo <jeremykatush@gmail.com>
2024-05-23 15:38:46 +0300

    added type assertion in UserContext component

--- src/components/context/UserC.tsx
+++ src/components/context/UserC.tsx
@@ -6,26 +6,26 @@ export const UserC = () => {
     const userContext = useContext(UserContext)
 
     const handleLogin = () => {
-        if (userContext) {
+        // if (userContext) {
             userContext.setUser({
                 name: 'Jane Doe',
                 email: 'janedoe@gmail.com'
             })
-        }
+        // }
     }
 
     const handleLogout = () => {
-        if (userContext) {
+        // if (userContext) {
             userContext.setUser(null)
-        }
+        // }
     }
 
     return (
         <div>
             <button onClick={handleLogin}>Login</button>
             <button onClick={handleLogout}>Logout</button>
-            <div>Your name is: {userContext?.user?.name}</div>
-            <div>Your email is: {userContext?.user?.email}</div>
+            <div>Your name is: {userContext.user?.name}</div>
+            <div>Your email is: {userContext.user?.email}</div>
         </div>
     )
 }
