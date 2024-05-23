d04e1c9439247c6f8988f76b746840e1654e8293
Jeremiah-Katumo <jeremykatush@gmail.com>
2024-05-23 13:55:26 +0300

    added code in Counter

--- src/components/state/Counter.tsx
+++ src/components/state/Counter.tsx
@@ -14,3 +14,18 @@ function reducer(state, action) {
     }
 }
 
+export const Counter = () => {
+    const [state, dispatch] = useReducer(reducer, initialState)
+
+    return (
+        <>
+            Count: {state.count}
+            <button onClick={() => dispatch({ type: 'increment', payload: 10 })}>
+                Increment 10
+            </button>
+            <button onClick={() => dispatch({ type: 'decrement', payload: 10 })}>
+                Decrement 10
+            </button>
+        </>
+    )
+}
