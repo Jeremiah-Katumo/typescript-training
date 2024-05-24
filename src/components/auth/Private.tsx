db47ced82d137ea3e5f8e5f002e6672d924da009
Jeremiah-Katumo <jeremykatush@gmail.com>
2024-05-24 13:19:54 +0300

    added code in Private

---
+++ src/components/auth/Private.tsx
@@ -0,0 +1,10 @@
+
+import { Login } from './Login'
+
+export const Private = ({ isLoggedIn, Component }) => {
+    if (isLoggedIn) {
+        return <Component />
+    } else {
+        return <Login />
+    }
+}
