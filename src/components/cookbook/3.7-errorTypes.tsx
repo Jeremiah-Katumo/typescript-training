91aed9fe2c3d033da3fdd88d74e4425b93c5c759
Jeremiah-Katumo <jeremykatush@gmail.com>
2024-07-11 12:24:17 +0300

    try catch promise

---
+++ src/components/cookbook/3.7-errorTypes.tsx
@@ -0,0 +1,47 @@
+
+try {
+    myroutine()
+} catch (e) {
+    if (e instanceof TypeError) {
+
+    } else if (e instanceof EvalError) {
+
+    } else if (typeof e === "string") {
+
+    } else {
+        console.log(e)
+    }
+}
+
+
+
+const somePromise = () => new Promise((fulfill, reject) => {
+    if (someConditionIsValid()) {
+        fulfill(42)
+    } else {
+        reject("Oh no!")
+    }
+})
+
+somePromise()
+    .then((val) => console.log(val))
+    .catch((e) => console.log(e))
+
+// It becomes clearer if you call the same Promise in an async/await flow:
+try {
+    const z = await somePromise();
+} catch(e) {
+    console.log(e)
+}
+
+export {}
+
+// If you want to define your own errors and catch accordingly, you can either 
+// write error classes and do instance of checks or create helper functions 
+// that check for certain properties and tell the correct type via type predicates. 
+// Axios is again a good example for that:
+function isAxiosError(payload: any): payload is AxiosError { 
+    return payload !== null 
+        && typeof payload === 'object' 
+        && payload.isAxiosError;
+}
