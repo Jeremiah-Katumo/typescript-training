47443525245f115890c99f1ef74b8fa959eb8b3b
Jeremiah-Katumo <jeremykatush@gmail.com>
2024-07-11 15:19:38 +0300

    how to add generics

---
+++ src/components/cookbook/4-generics/4.1-functionSignatures.tsx
@@ -0,0 +1,84 @@
+
+// Problem: You have two functions that work the same, but on different and largely
+// incompatible types
+
+// Solution: Generalize their behavior using generics.
+
+type Languages = { 
+    de: URL; 
+    en: URL; 
+    pt: URL; 
+    es: URL; 
+    fr: URL; 
+    ja: URL;
+};
+
+function isLanguageAvailable(
+    collection: Languages,
+    lang: string
+): lang is keyof Languages {
+    return lang in collection
+}
+
+function loadLanguage(
+    collection: Languages, 
+    lang: string
+) {
+    if (isLanguageAvailable(collection, lang)) {
+        collection[lang]
+    }
+}
+
+
+
+type AllowedElements = { 
+    video: HTMLVideoElement; 
+    audio: HTMLAudioElement; 
+    canvas: HTMLCanvasElement;
+};
+
+function isElementAllowed( 
+    collection: AllowedElements, 
+    elem: string
+): elem is keyof AllowedElements { 
+    return elem in collection;
+}
+
+function selectElement(
+    collection: AllowedElements, 
+    elem: string
+) { 
+    if (isElementAllowed(collection, elem)) { 
+        // elem is keyof AllowedElements 
+        collection[elem]; // access ok
+    } 
+}
+
+
+// We can use this generic type parameter like we would use AllowedElements or Languages 
+// and can add a type predicate. Since Obj can be substituted for every type, key needs 
+// to include all possible property keys— string, symbol, and number:
+function isAvailable<Obj>( 
+    obj: Obj, 
+    key: string | number | symbol
+): key is keyof Obj { 
+    return key in obj;
+}
+function loadLanguageTwo(
+    collection: Languages, 
+    lang: string
+) { 
+    if (isAvailable(collection, lang)) { 
+        // lang is keyof Languages 
+        collection[lang]; // access ok!
+    } 
+}
+function selectElementTwo(
+    collection: AllowedElements, 
+    elem: string
+) { 
+    if (isAvailable(collection, elem)) { 
+        // elem is keyof AllowedElements 
+        collection[elem]; // access ok
+    } 
+}
